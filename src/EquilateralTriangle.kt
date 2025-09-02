e8fade77a2a5dec9bd417efa4364d1bb1b0b03c9
Wyatt Zantua <tuo18351@temple.edu>
2025-09-02 17:08:55 -0400

    Revised EquilateralTriangle functions to utilize superclass methods

--- src/EquilateralTriangle.kt
+++ src/EquilateralTriangle.kt
@@ -1,19 +1,15 @@
-import kotlin.math.sqrt
-import kotlin.math.pow
-
 class EquilateralTriangle (_name : String = "Equilateral Triangle"): Triangle(_name)  {
     private var side : Double = 0.0
 
     fun setDimensions(side : Double) {
-        this.side = side
+        super.setDimensions(side, side, side)
     }
 
     override fun printDimensions() {
-        println("Side Length: $side")
+        super.printDimensions()
     }
 
     override fun getArea(): Double {
-        val x = 1.5 * (side)
-        return sqrt((x-side).pow(3) * x)
+        return super.getArea()
     }
 }
